c499247811455e97cbddc0e9c46c9a04a7f8bb6d
yap324 <fef007093@gmail.com>
2026-02-17 01:39:15 +0700

    Update FUCKME.lua

--- FUCKME.lua
+++ FUCKME.lua
@@ -101,7 +101,7 @@ getgenv().ExunysDeveloperAimbot = {
 		WallCheck = false,
 
 		OffsetToMoveDirection = true,
-		OffsetIncrement = 15,
+		OffsetIncrement = 5,
 
 		Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
 		Sensitivity2 = 3.5, -- mousemoverel Sensitivity
